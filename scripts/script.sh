2b00401d0940d0a4afb9c5e32670e20813e83e76
Jakub Wilgocki-Slezak <jakub.wilgocki@gmail.com>
2023-01-18 14:22:18 +0100

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,7 +2,7 @@
 
 echo "I'm running from the script"
 
-if [[ $AD_PASS -z ]]; then echo "pusta zmienna"
+if [[ -z "$AD_PASS" ]]; then echo "pusta zmienna"
 fi
 
 if [[ $1 == "123456:password" ]]; then
