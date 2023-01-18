522bf3dfd71b8752ce1edb093210efe77522df7c
Jakub Wilgocki-Slezak <jakub.wilgocki@gmail.com>
2023-01-18 14:09:26 +0100

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,11 +1,11 @@
 
 echo "I'm running from the script"
-if [[ ${env.AD_PASS} == "123456:password" ]]; then
+if [[ ${AD_PASS} == "123456:password" ]]; then
   echo "podany credential to 123456:password"
 fi
 
 
-if [[ ${env.AD_PASS_PSW} == "password" ]]; then
+if [[ ${AD_PASS_PSW} == "password" ]]; then
   echo "haslo sie zgadza"
  else
  echo "cos nie smiga"
