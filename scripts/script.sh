7f631324f6893f8e57995c38349947d8ae920f19
Jakub Wilgocki-Slezak <jakub.wilgocki@gmail.com>
2023-01-18 13:33:49 +0100

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,2 +1,12 @@
 
 echo "I'm running from the script"
+if [[ $AD_PASS == "123456:password" ]]; then
+  echo "podany credential to 123456:password"
+fi
+
+
+if [[ $AD_PASS_PSW == "password" ]]; then
+  echo "haslo sie zgadza"
+ else
+ echo "cos nie smiga"
+ fi
